026e589dc79f02475c2ad79ded1318eb115b4ed4
u34v7 <u34v7@tichon.guttman>
2017-12-10 09:26:18 +0200

    More code

--- commands/iam-config.js
+++ commands/iam-config.js
@@ -1,6 +1,5 @@
 exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
-  const msg = await message.channel.send("Ping?");
-  msg.edit(`Pong! Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+	
 };
 
 exports.conf = {
@@ -14,5 +13,5 @@ exports.help = {
   name: "iam-config",
   category: "System",
   description: "Changes the list of roles available with the /iam command.";
-  usage: "iam-config <add/edit/del> <rolename> <@role>"
+  usage: "iam-config <add/del> <rolename> <@role>"
 };
